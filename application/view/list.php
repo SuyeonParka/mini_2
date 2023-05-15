2fc9a3d6fdd805fb0abc53ac3756ba208718799b
SuyeonParka <sysysy3331@gmail.com>
2023-05-15 12:47:33 +0900

    0515

--- application/view/list.php
+++ application/view/list.php
@@ -4,11 +4,197 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>List</title>
 </head>
 <body>
-    List File!!!!!
-    <button id="logout" onclick="redirectLogout();">Logout</button>
+    <!-- List File!!!!! -->
+    <!-- <button id="logout" onclick="redirectLogout();">Logout</button> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">OTIE</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </li>
+            
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">
+                    Login
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" id="logout" onclick="redirectLogout();">
+                    Logout
+                </a>
+            </li>
+            </ul>
+            <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-light" type="submit">Search</button>
+            </form>
+        </div>
+        </div>
+    </nav>
+
+    <!-- 카드(grid) -->
+    <div class="container">
+        <div class="row row-cols-xxl-4">
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem;  float:none; margin:0 auto; border:none;">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678822932889_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTILING HAND BAG</h5>
+                    <p class="card-text">DENIM CLASSIC</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678823745299_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTLING SHOULDER BAG</h5>
+                    <p class="card-text">DENIM SKY</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678822983086_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTILING HAND BAG</h5>
+                    <p class="card-text">DENIM HAIRY</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678823008427_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTLING HAND BAG</h5>
+                    <p class="card-text">SILVER</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678822572282_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTLING HAND BAG</h5>
+                    <p class="card-text">BLUSH PINK</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678822368713_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTLING HAND BAG</h5>
+                    <p class="card-text">PEARL CREAM</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678824149036_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTLING CROSS BAG MINI</h5>
+                    <p class="card-text">SILVER</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center pt-3 pb-3">
+                <div class="card" style="width: 18rem; float:none; margin:0 auto; border:none">
+                    <img src="https://contents.sixshop.com/thumbnails/uploadedFiles/109465/product/image_1678823978095_1000.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">RUSTLING CROSS BAG MINI</h5>
+                    <p class="card-text">BLUSH PINK</p>
+                    <button type="button" class="btn btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        BUY/CART
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- 모달 -->
+    <!-- Button trigger modal -->
+    <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    구매하기
+    </button> -->
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">BUY</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+            <div class="modal-body">
+                LIST
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">CANCLE</button>
+                    <button type="button" class="btn btn-dark">CART</button>
+                    <button type="button" class="btn btn-dark">BUY</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <footer class="container-fluid navbar-fixed-bottom">
+        <span>상호: 오타이 | 대표: 이예임 | 개인정보관리책임자: 이예임 | 전화: 010-0000-0000 | 이메일: otie-000@naver.com
+            
+            주소: 서울특별시 강북구 00로 000, 2층 (번동) | 사업자등록번호: 000-00-00000 | 통신판매: 0000-서울강북-0000 호 | 호스팅제공자: (주)00샵
+            <br>
+            <span class="foot">이용약관 개인정보처리방침 사업자정보확인</span>
+            <br>
+            Otie
+        </span>
+    </footer>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 
 
     <script>
--- application/view/login.php
+++ application/view/login.php
@@ -4,18 +4,109 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/login.css">
     <title>Login</title>
+    <style>
+        @font-face {
+    font-family: 'PuradakGentleGothicR';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_11-01@1.1/PuradakGentleGothicR.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    font-family: 'PuradakGentleGothicR';
+    display: grid;
+    justify-content: space-around;
+}
+
+a {
+    text-decoration: none;
+    color: #000;
+}
+
+.container {
+    width: 50vw;
+    height: 30vh;
+    display: grid;
+    place-items: center;
+    min-height: 100vh;
+}
+
+h1 {
+    margin: 30px;
+}
+
+.btn {
+    font-family: 'PuradakGentleGothicR';
+    font-weight: 600;
+    font-size: 0.3rem;
+    border-radius: 5px;
+    border: 1px solid #000;
+    width: 170px;
+    height: 3vh;
+    background-color: #ecedf0;
+}
+
+span {
+    font-size: 0.5rem;
+}
+
+.stick {
+    margin-left: 15px;
+    margin-right: 15px;
+}
+
+.stay {
+    font-size: 0.3rem;
+}
+    </style>
 </head>
 <body>
+    <!--  
     <h1>Login</h1>
-    <h3 style="color: red;"><?php echo isset($this->errMsg) ? $this->errMsg : ""; ?></h3>
-    <form action="/user/login" method="post">  <!-- /붙여줘야함(url불어남 방지) -->
+    <h3 style="color: red;">
+        <?php echo isset($this->errMsg) ? $this->errMsg : ""; ?>
+    </h3>
+    <form action="/user/login" method="post">  /붙여줘야함(url불어남 방지) 
         <label for="id">ID</label>
         <input type="text" name="id" id="id">
         <label for="pw">PW</label>
         <input type="text" name="pw" id="pw">
         <button type="submit">Login</button>
     </form>
+-->
+<div class="container">
+    <form class="my_form" method="post" action="/user/login">
+        <h1>Login</h1>
+        <input type="text" name = "id" id="log_id" placeholder="id" required>
+        <br>
+        <br>
+        <input type="password" name = "pw" id="log_pw" placeholder="password" required>
+        <br>
+        <br>
+        <span>
+            <input style='zoom:0.7;' type="checkbox" class="stay">
+            로그인 상태 유지
+        </span>
+        <br>
+        <span style="color: red; font-size=10px"><?php echo isset($this->errMsg) ? $this->errMsg : ""; ?></span>
+        <br>
+        <br>
+        <input type="submit" value="Login" class="btn">
+        <br>
+        <br>
+        <span><a href="">아이디 저장하기</a></span>
+        <span class="stick">|</span>
+        <span><a href="">ID/PW 찾기 </a></span>
+    </form>
+</div>
 
     
 </body>
