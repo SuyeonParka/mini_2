b40fead4d0ab95b9074c4015414f1879166a30c2
SuyeonParka <sysysy3331@gmail.com>
2023-05-17 00:07:44 +0900

    0517

--- application/lib/config.php
+++ application/lib/config.php
@@ -18,12 +18,12 @@ define("_DB_CHARSET", "utf8mb4");
 // 기타
 define("_EXTENSION_PHP", ".php");
 
-
+//PATH : 경로
 define("_PATH_CONTROLLER", "application/controller/");
 define("_PATH_MODEL", "application/model/");
 define("_PATH_VIEW", "application/view/");
 
-
+// BASE : 기본(부모) 파일
 define("_BASE_FILENAME_CONTROLLER", "Controller");
 define("_BASE_FILENAME_MODEL", "Model");
 
@@ -32,6 +32,7 @@ define("_BASE_REDIRECT", "Location: ");
 
 define("_STR_LOGIN_ID", "u_id");
 
+// HEADER/FOOTER 경로
 define("_HEADER", "application/view/header");
 define("_FOOTER", "application/view/footer");
 
